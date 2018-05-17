027455cd0f21c3adcdf3b445bd935eca9b5ea128
Shunsuke Okumura <shun.2fb.wb7@gmail.com>
2018-05-18 00:51:36 +0900

    set max Y as 100

--- app01.js
+++ app01.js
@@ -8,14 +8,14 @@ app.controller('formCtrl', function () {
 	//グラフの値
 	me.items = [
 		//{値,月度}
-		{num:120,month:1}, 
-		{num:220,month:2}, 
-		{num:332,month:3}, 
-		{num:210,month:4}, 
-		{num:460,month:5}, 
+		{num:120,month:1},
+		{num:220,month:2},
+		{num:332,month:3},
+		{num:210,month:4},
+		{num:460,month:5},
 		{num:520,month:6}
 	];
- 
+
 	//値を配列に入れ替えてから、Chart.jsへ渡す
 	var addData = (function(){
 		var arr = [];
@@ -25,10 +25,15 @@ app.controller('formCtrl', function () {
 		return arr;
 	}());
 	chartData.datasets[0].data = addData;
- 
+
 	//グラフを生成(Line:折れ線グラフ)
-	var myChart = new Chart(ctx).Line(chartData);
- 
+	var myChart = new Chart(ctx).Line(chartData, {
+		scaleOverride : true,
+    scaleSteps : 10,
+    scaleStepWidth : 10,
+    scaleStartValue : 0
+	});
+
 	//フォームの値に変更があったら
 	me.change = function(formName,ind){
 		//フォームにエラーが無いかチェック
@@ -39,7 +44,7 @@ app.controller('formCtrl', function () {
 		myChart.update();
 	};
 });
- 
+
 //Chart.jsへ渡すデータ(Line:折れ線グラフ用)
 var chartData ={
 	labels: ["1月","2月","3月","4月","5月","6月"],//X軸のラベル
